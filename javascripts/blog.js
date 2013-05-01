902178693d5a7b383ad20ce0af2a3ad6fe517760
Colin Devroe <colin@cdevroe.com>
2013-05-01 19:14:08 -0400

    REmoved JS to adjust column heights

--- javascripts/blog.js
+++ javascripts/blog.js
@@ -1,15 +1,5 @@
 $(document).foundation();
 
-// Unify content column heights
-var height_of_left = $('#left-col').outerHeight();
-var height_of_right = $('#right-col').outerHeight();
-if (height_of_left > height_of_right) { 
-	height_of_tallest = height_of_left;
-} else {
-	height_of_tallest = height_of_right;
-}
-$('#right-col').css('height', height_of_tallest);
-
 // Clone sidebar to off-canvas location for mobile
 var $clonenav = $('#sideNav').clone();
 $("#sideMenu").append($clonenav);
