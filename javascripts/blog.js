922fd06c961c352ceec8f3c18e59a7e3b09d4419
Colin Devroe <colin@cdevroe.com>
2013-05-23 16:52:26 -0400

    Added Photo Albums

--- javascripts/blog.js
+++ javascripts/blog.js
@@ -13,3 +13,14 @@ $("a[href='#top']").click(function() {
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
+
+/* Barley Editor Mode Only */
+// Used for picking different photo album layouts
+if ($('#album_tools').length > 0) {
+	$('#album_tools a').click(function(e){
+		e.preventDefault();
+		photo_album_chosen = $(this).attr('data-album');
+		photo_album_layout = $('#album-'+photo_album_chosen).html();
+		$('#photo_album').html(photo_album_layout);
+	});
+}
