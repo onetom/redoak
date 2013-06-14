b737619df3ed73b92d0396603641624b5ff59387
Tim Whitacre <tim@argyllstudios.com>
2013-06-14 10:19:59 -0400

    Updated photos-single page to wrap seperate templates in owner and updated blog.js to reflect the new method available in the editor

--- javascripts/blog.js
+++ javascripts/blog.js
@@ -17,10 +17,12 @@ $("a[href='#top']").click(function() {
 /* Barley Editor Mode Only */
 // Used for picking different photo album layouts
 if ($('#album_tools').length > 0) {
-	$('#album_tools a').click(function(e){
-		e.preventDefault();
-		photo_album_chosen = $(this).attr('data-album');
-		photo_album_layout = $('#album-'+photo_album_chosen).html();
-		$('#photo_album').html(photo_album_layout);
-	});
+	$('#album_tools a').on('click', function(e){
+        e.preventDefault();
+        var photo_album_chosen, photo_album_layout;
+        photo_album_chosen = $(this).attr('data-album');
+        photo_album_layout = $('#album-'+photo_album_chosen).html();
+        $('#photo_album').empty().html(photo_album_layout);
+        if (typeof BarleyTools != 'undefined') { BarleyTools('findImages'); }
+    });
 }
